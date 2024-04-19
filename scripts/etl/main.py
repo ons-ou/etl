9da5d86ac23b4ea457b20ce6aea0b46686da6c93
ons-ou <onsouahchi@gmail.com>
2024-04-19 18:41:52 +0100

    update main

--- scripts/etl/main.py
+++ scripts/etl/main.py
@@ -6,5 +6,5 @@ from scripts.etl.ZipWorflow import ZipWorkflow
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
 
 if __name__ == '__main__':
-    #ZipWorkflow().workflow_thread()
+    ZipWorkflow().workflow_thread()
     SeleniumWorkflow().workflow_thread()
